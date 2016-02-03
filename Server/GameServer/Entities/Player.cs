c6a6e44b2d8b36ce6b1714203ee278c03e1e7267
Daniel <danielmolenaar@hotmail.nl>
2016-02-03 22:58:54 +0100

    Fixed player issue

--- Server/GameServer/Entities/Player.cs
+++ Server/GameServer/Entities/Player.cs
@@ -11,7 +11,7 @@ namespace Lobby.Entities {
         internal bool IsHost { get; set; }
         internal bool Ready { get; set; }
         internal bool GameLoaded { get; set; }
-        internal bool IsAlive { get; set; }
+        internal bool IsAlive { get; set; } = true;
 
         internal TcpClient TcpClient { get; }
         internal IPEndPoint ClientIp { get; }
